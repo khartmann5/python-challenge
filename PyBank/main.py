6d903de22f2e0cb32e680e2d1438695bc2962c4f
khartmann5 <hartmann.kt@gmail.com>
2021-01-07 21:38:17 -0800

    saving changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,9 +9,7 @@ import csv
 
 budget_data = os.path.join('Resources','budget_data.csv')
 
-# Set initial variables
-
-# Define lists
+# Define variables
 total = 0
 
 # read CSV file
@@ -31,6 +29,14 @@ with open(budget_data, newline='') as csvfile:
     # Sum the net total amount of "Profit/Losses" over the period
         total += int(row[1])
         print(f"Total: ${total}")
+
+pybank_financial_analysis = os.path.join("pybank_financial_analysis.txt")
+with open(pybank_financial_analysis,"w", newline='') as outfile:
+
+    outfile.writerow("Financial Analysis")
+    outfile.writerow("---------------------------")
+    outfile.writerow("Months: " + months)
+
 
        
 
