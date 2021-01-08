efc0aec04a5dccf62c18f8390ca33d5da78dfeac
khartmann5 <hartmann.kt@gmail.com>
2021-01-07 18:40:47 -0800

    saving work

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,16 +9,23 @@ import csv
 
 csvpath = os.path.join('Resources','budget_data.csv')
 
+# Define total
+total = 0
+
 # read CSV file
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # Read the header row first
+    #read header row
     csv_header = next(csvreader)
-
+    
     # Read each row of data
     for row in csvreader:
         
         # Count number of months
-        months = len(list(csvreader))
-        print(f"Total Months: {months}")        
+        months = len(list(csvreader))+1
+        print(f"Total Months: {months}")     
+
+        # Sum the net total amount of "Profit/Losses" over the period and define total
+        total += float(row[1])
+        print(f"Total: ${total}")
