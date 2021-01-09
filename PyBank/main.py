08513457fdb33f93c196b9f5dea246056647a9d9
khartmann5 <hartmann.kt@gmail.com>
2021-01-08 18:55:31 -0800

    saving changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,7 +15,10 @@ current_profit_loss = 0
 previous_profit_loss = 0
 profit_loss_change = 0
 
+# Lists to store data
 profit_loss_changes = []
+current_profit_losses = []
+net_profit_losses = []
 
 # Read CSV file
 with open(budget_data, newline='') as csvfile:
@@ -31,9 +34,21 @@ with open(budget_data, newline='') as csvfile:
     # Read each row of data
     for row in csvreader:
         
-        # Sum the net total amount of "Profit/Losses" over the period
-        net_profit_loss += int(row[1])
-    print(f"Total: ${net_profit_loss}")
+        # Append the net total amount of "Profit/Losses" over the period
+        net_profit_losses.append(int(row[1]))
+
+        # Determine current profit loss
+        # current_profit_loss = net_profit_loss
+
+        # # Calculate previous months profit loss
+        # previous_profit_loss = current_profit_loss(row[1]-1)
+
+# Sum total of net profit losses
+net_profit_losses = sum(net_profit_losses)
+
+print(net_profit_losses)
+
+    
 
     
 
@@ -43,12 +58,12 @@ with open(budget_data, newline='') as csvfile:
         
       
 # write to text analysis file
-pybank_financial_analysis = os.path.join("pybank_financial_analysis.txt")
-with open(pybank_financial_analysis,"w") as outfile:
+# pybank_financial_analysis = os.path.join("pybank_financial_analysis.txt")
+# with open(pybank_financial_analysis,"w") as outfile:
     
-    outfile.write("Financial Analysis\n")
-    outfile.write("---------------------------\n")
-    outfile.write(f"Total Months: {months}")
+#     outfile.write("Financial Analysis\n")
+#     outfile.write("---------------------------\n")
+#     outfile.write(f"Total Months: {months}")
 
 
        
