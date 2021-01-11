44f614a61bcf39b799469f7b681fd8a710bd81e8
khartmann5 <hartmann.kt@gmail.com>
2021-01-10 18:44:37 -0800

    adding final pypoll code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,7 +8,6 @@ election_data = os.path.join('Resources','election_data.csv')
 
 # Define variables
 total_votes = 0
-vote_candidate = []
 candidates_dict = {}
 vote_percentages = {}
 
@@ -34,32 +33,29 @@ with open(election_data, newline='') as csvfile:
 
 # Calculate percentage won
 for key in candidates_dict.keys():
-    vote_candidate.append(candidates_dict[key]/total_votes * 100)
-    vote_percentages = vote_candidate
-
-print(vote_percentages)
+    vote_percentages[key] = round((candidates_dict[key]/total_votes * 100),1)
 
 # Print results
 print("Election Results\n-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
 for key in candidates_dict:   
-    print(f"{key}: {vote_percentages[0]}% ({candidates_dict[key]})")
+    print(f"{key}: {vote_percentages[key]}% ({candidates_dict[key]})")
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
 # write to text analysis file
-# pypoll_election_results = os.path.join("pypoll_election_results.txt")
-# with open(pypoll_election_results,"w") as outfile:
+pypoll_election_results = os.path.join("pypoll_election_results.txt")
+with open(pypoll_election_results,"w") as outfile:
     
-#     outfile.write("Election Results\n-------------------------\n")
-#     outfile.write(f"Total Votes: {total_votes}\n")
-#     outfile.write("-------------------------\n")
-#     for key in candidates_dict:   
-#         outfile.write(f"{key}: {vote_candidate[0]}% ({candidates_dict[key]})\n")
-#     outfile.write("-------------------------\n")
-#     outfile.write(f"Winner: {winner}\n")
-#     outfile.write("-------------------------")
+    outfile.write("Election Results\n-------------------------\n")
+    outfile.write(f"Total Votes: {total_votes}\n")
+    outfile.write("-------------------------\n")
+    for key in candidates_dict:   
+        outfile.write(f"{key}: {vote_percentages[key]}% ({candidates_dict[key]})\n")
+    outfile.write("-------------------------\n")
+    outfile.write(f"Winner: {winner}\n")
+    outfile.write("-------------------------")
 
 
